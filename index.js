ebca81cfb2a7253d74e2aa7163985fc7ee5eb5db
Jonas Brusman <himynameisjonas@gmail.com>
2019-04-14 15:32:35 +0200

    More styling

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import './app.scss'
+import fitty from 'fitty';
+
+document.documentElement.classList.add('js');
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(function(){
+  fitty('.fit');
+})
+
+
